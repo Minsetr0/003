1c308952dfb5442af73d22e86ac959bad59f7cf6
Minsetr0 <nesterovmaksim71@mail.ru>
2023-04-20 00:24:43 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Random random = new Random();
+int[] array = new int[30];
+int firstNumberInArray = 0;
+int maxNumberInArray = 31;
+int minNumberInArray = 0;
+int rightNeighbor = 1;
+int leftNeighbor = -1;
+
+Console.Write("Все локальные максимумы: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(minNumberInArray, maxNumberInArray);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 0)
+    {
+        if (array[i] > array[i + rightNeighbor])
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    else if (i != firstNumberInArray && i != array.Length)
+    {
+        if (array[i] > array[i + leftNeighbor] && array[i] > array[i + rightNeighbor])
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+    else if (i == array.Length)
+    {
+        if (array[i] > array[i + leftNeighbor])
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
